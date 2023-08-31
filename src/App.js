813afddb177bfba95916a28a70a461095d80184e
lioma-is <lyomaikt@gmail.com>
2023-08-31 09:46:08 +0300

    react_27u_1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Frontend12_lyoma
         </a>
       </header>
     </div>
